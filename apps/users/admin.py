0274308e8d8c573eb4cbc9fa9cd097b34aecf3f8
Tomas Nardi <tomas.nardi@hotmail.com>
2026-05-11 16:16:29 -0300

    feat(admin): enhance permissions layout and responsiveness in auth group settings

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,8 +1,43 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import Group
 from unfold.admin import ModelAdmin
 
 from .models import User
+
+
+class GroupAdminForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        permissions_field = self.fields.get("permissions")
+        if permissions_field is not None:
+            permissions_field.help_text = (
+                "Selecciona permisos y usa las flechas para moverlos."
+            )
+            permissions_field.widget.attrs.update({"size": 24})
+
+
+try:
+    admin.site.unregister(Group)
+except admin.sites.NotRegistered:
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(ModelAdmin, BaseGroupAdmin):
+    form = GroupAdminForm
+    change_form_template = "admin/auth/group/change_form.html"
+
+    class Media:
+        css = {
+            "all": ("admin/css/group_admin.css",),
+        }
 
 
 @admin.register(User)
